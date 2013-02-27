a945a00a192702a46a70767c6c206befa47a2ab1
vincent.hervieux <vincent.hervieux@gmail.com>
2013-02-27 23:20:42 +0000

    bug 12: MessageBoxes About and Credits are badly formatted

--- src/oscilloscope.h
+++ src/oscilloscope.h
@@ -35,32 +35,33 @@
 /*!!! TODO remove this flag while testing with HW!!!*/
 //#define TEST_WITHOUT_HW
 
-#define ABOUT_QPICOSCOPE     "<p align='center'>Copyright 2012 Vincent HERVIEUX<br>"\
+#define ABOUT_QPICOSCOPE     "<p align='center'>Copyright&nbsp;2012&nbsp;Vincent&nbsp;HERVIEUX<br>"\
                              "<a href='mailto:vincent.hervieux@free.fr?Subject=QPicoscope'>vincent.hervieux@free.fr</a></p>"\
                              "<p align='justify'><br>"\
-                             "Qpicoscope is a Qt frontend for Picoscope 2000-3000 series<br>"\
-                             "from <a href='http://www.picotech.com'>Pico Tech</a>.<br>"\
+                             "Qpicoscope&nbsp;is&nbsp;a&nbsp;Qt&nbsp;frontend&nbsp;for&nbsp;Picoscope&nbsp;2000-3000&nbsp;series&nbsp;"\
+                             "from&nbsp;<a href='http://www.picotech.com'>Pico&nbsp;Tech</a>.<br>"\
                              "<br>"\
-                             "QPicoscope is free software: you can redistribute it and/or modify<br>"\
-                             "it under the terms of the GNU Lesser General Public License as published by<br>"\
-                             "the Free Software Foundation, either version 3 of the License, or<br>"\
+                             "QPicoscope is free software:<br>"\
+                             "you can redistribute it and/or modify "\
+                             "it under the terms of the GNU Lesser General Public License as published by "\
+                             "the Free Software Foundation, either version 3 of the License, or "\
                              "any later version.<br>"\
                              "<br>"\
-                             "QPicoscope is distributed in the hope that it will be useful,<br>"\
-                             "but WITHOUT ANY WARRANTY; without even the implied warranty of<br>"\
-                             "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>"\
+                             "QPicoscope is distributed in the hope that it will be useful, "\
+                             "but WITHOUT ANY WARRANTY; without even the implied warranty of "\
+                             "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "\
                              "GNU Lesser General Public License for more details.<br>"\
                              "<br>"\
-                             "You should have received a copy of the GNU Lesser General Public License<br>"\
+                             "You should have received a copy of the GNU Lesser General Public License "\
                              "along with QPicoscope in files COPYING.LESSER and COPYING.<br>"\
                              "If not, see <a href='http://www.gnu.org/licenses'>http://www.gnu.org/licenses</a>.<br></p>"
 
-
 #define CREDITS_QPICOSCOPE   "<p align='justify'>QPicoscope is using:<br>"\
-                             "- Qt. Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).<br>"\
-                             "  Qt is a Nokia product. See qt.nokia.com for more information.<br>"\
-                             "- Qwt. Copyright (C) 1997 Josef Wilgen; Copyright (C) 2002 Uwe Rathmann.<br>"\
-                             "- AutoTroll. Copyright (C) 2006  Benoit Sigoure <benoit.sigoure@lrde.epita.fr.</p>"
+                             "-&nbsp;Qt.&nbsp;Copyright&nbsp;(C)&nbsp;2012&nbsp;Nokia&nbsp;Corporation&nbsp;and/or&nbsp;its&nbsp;subsidiary(ies).<br>"\
+                             "&nbsp;&nbsp;Qt&nbsp;is&nbsp;a&nbsp;Nokia&nbsp;product.&nbsp;See&nbsp;qt.nokia.com&nbsp;for&nbsp;more&nbsp;information.<br>"\
+                             "-&nbsp;Qwt.&nbsp;Copyright&nbsp;(C)&nbsp;1997&nbsp;Josef&nbsp;Wilgen;&nbsp;Copyright&nbsp;(C)&nbsp;2002&nbsp;Uwe&nbsp;Rathmann.<br>"\
+                             "-&nbsp;AutoTroll.&nbsp;Copyright&nbsp;(C)&nbsp;2006&nbsp;&nbsp;Benoit&nbsp;Sigoure&nbsp;<benoit.sigoure@lrde.epita.fr>.</p>"
+
 typedef enum
 {
     E_CURRENT_AC = 0,
